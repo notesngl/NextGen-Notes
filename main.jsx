8f9bde933ca0eeb54a9edac50d27e47a74834ab0
notesngl <nextgenlearningrjl@gmail.com>
2026-08-26 17:05:30 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminPanel from './AdminPanel.jsx'
+
+const isAdmin = window.location.pathname === '/admin'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <AdminPanel /> : <App />}
   </React.StrictMode>,
 )
